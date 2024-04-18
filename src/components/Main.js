d545c7d4e1611cc88d87015c8e97da0723f38c78
cerendn <cerendncer@outlook.com>
2024-04-18 21:56:16 +0300

    finish

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,16 +2,20 @@ import React from "react";
 import { useState } from "react";
 
 function Main() {
+  const [imgUrl, setImgUrl] = useState("");
+  const callImg = () => {
+    fetch("https://api.thecatapi.com/v1/images/search")
+      .then((res) => res.json())
+      .then((data) => setImgUrl(data[0].url));
+  };
+
   return (
     <>
       <div className="container">
         <div className="box">
           <h2>Enjoy the kittie</h2>
-          <img
-            src="https://thumbor.bigedition.com/cutest-cat-breeds/2R5kNego4yesF2nMDFFzA3qbK_w=/800x0/filters:quality(80)/granite-web-prod/e6/14/e614c47d95c440d399c2c1bc26458b38.jpeg"
-            alt=""
-          />
-          <button>Click Me</button>
+          <img src={imgUrl} alt="" />
+          <button onClick={() => callImg()}>Click Me</button>
         </div>
       </div>
     </>

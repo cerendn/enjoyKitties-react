47b2a4611c331eb32d34b2830aece97a26cccf57
cerendn <cerendncer@outlook.com>
2024-04-18 21:30:13 +0300

    start

---
+++ src/components/Main.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useState } from "react";
+
+function Main() {
+  return (
+    <>
+      <div className="container">
+        <div className="box">
+          <h2>Enjoy the kittie</h2>
+          <img
+            src="https://thumbor.bigedition.com/cutest-cat-breeds/2R5kNego4yesF2nMDFFzA3qbK_w=/800x0/filters:quality(80)/granite-web-prod/e6/14/e614c47d95c440d399c2c1bc26458b38.jpeg"
+            alt=""
+          />
+          <button>Click Me</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Main;
